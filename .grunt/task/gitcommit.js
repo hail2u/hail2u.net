067ca662c603bb1efc85b90c23b37e5e67aaa396
Kyo Nagashima <kyo@hail2u.net>
2014-10-22 10:37:53 +0900

    Initialize `all` option

--- .grunt/task/gitcommit.js
+++ .grunt/task/gitcommit.js
@@ -10,6 +10,7 @@ module.exports = function (grunt) {
 
     var done = this.async();
     var options = this.options({
+      all: false,
       branch: 'master',
       message: 'Commit',
       push: false,
