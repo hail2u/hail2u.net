1f9b47018a9cf7c390011d913766261c96e25217
Kyo Nagashima <kyo@hail2u.net>
2014-12-06 03:21:52 +0900

    Keep original tagline HTML, and use it

--- src/js/debug/toggle-tagline.js
+++ src/js/debug/toggle-tagline.js
@@ -1,12 +1,15 @@
-document.querySelector('.tagline').addEventListener('click', function () {
+(function () {
   var debug  = 'Lorem ipsum dolor sit amet, consectetur <em>adipiscing</em> elit. Cras sit amet risus ac odio porta sodales. Etiam consectetur eros et lacus &amp; tristique <em>imperdiet</em>.';
   var store = 'data-original';
-  var current = this.getAttribute(store);
-  this.setAttribute(store, this.innerHTML);
+  var tagline = document.querySelector('.tagline');
+  tagline.setAttribute(store, tagline.innerHTML);
+  tagline.addEventListener('click', function () {
+    var current = this.innerHTML;
 
-  if (current && current !== debug) {
-    this.innerHTML = current;
-  } else {
-    this.innerHTML = debug;
-  }
-}, false);
+    if (current !== debug) {
+      this.innerHTML = debug;
+    } else {
+      this.innerHTML = this.getAttribute(store);
+    }
+  }, false);
+})();
