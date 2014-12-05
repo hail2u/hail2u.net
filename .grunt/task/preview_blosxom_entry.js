1af99a41ddcad88302f2f5fdd921d0afcb796876
Kyo Nagashima <kyo@hail2u.net>
2014-12-05 21:26:17 +0900

    Revert "Delete `blosxom_preview` Grunt task"
    This reverts commit 7784674ea6aec0c24d0c42c46471d1fd35639f1a.

---
+++ .grunt/task/preview_blosxom_entry.js
@@ -0,0 +1,109 @@
+'use strict';
+
+module.exports = function (grunt) {
+  var taskName = 'preview_blosxom_entry';
+  var taskDescription = 'Preview Blosxom entry in default browser.';
+
+  grunt.registerTask(taskName, taskDescription, function () {
+    var fs = require('fs');
+    var marked = require('marked');
+    var path = require('path');
+
+    var done = this.async();
+    var file = grunt.option('file');
+    var preview = (function () {/*
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+
+    <title><%TITLE%> - Weblog - hail2u.net</title>
+
+    <base href="<%ROOT%>">
+
+    <link href="./favicon.ico" rel="icon">
+    <link href="./styles/style.min.css" rel="stylesheet">
+    <link href="./fonts/megrim.min.css" rel="stylesheet">
+    <link href="./fonts/source-code-pro.min.css" rel="stylesheet">
+    <link href="./fonts/source-sans-pro.min.css" rel="stylesheet">
+  </head>
+
+  <body class="permalink">
+    <header class="global-header" role="banner">
+      <h1 class="logo"><a href="./"><img src="./images/logo.min.svg" alt="Hail2u.net"></a></h1>
+
+      <nav class="site-navigation">
+        <ul>
+          <li><a href="./blog/"><mark>Weblog</mark></a></li>
+          <li><a href="./documents/">Documents</a></li>
+          <li><a href="./projects/">Projects</a></li>
+          <li><a href="./about/">About</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="main" role="main">
+      <article id="<%FN%>">
+        <h1 class="article-heading"><%TITLE%></h1>
+
+        <footer class="article-footer">
+          <p>on <time pubdate datetime="1976-07-23">Jul 23, 1976</time> under <span class=tag"><a href="#">Preview</a></span></p>
+        </footer>
+
+        <%BODY%>
+      </article>
+    </main>
+
+    <aside class="subcontent" role="search">
+      <!-- Google Custom Search Engine -->
+      <form class="searchbox searchboxbygoogle" action="http://www.google.com/cse">
+        <input type="hidden" name="cx" value="partner-pub-8712792805045949:3747342316">
+        <input type="hidden" name="ie" value="UTF-8">
+        <input type="search" name="q" size="32">
+        <input type="submit" name="sa" value="Search">
+      </form>
+    </aside>
+
+    <footer class="global-footer" role="contentinfo">
+      <section class="footlinks">
+        <ul>
+          <li><a href="http://creativecommons.org/licenses/by-nc/3.0/" rel="license">CC BY-NC</a></li>
+          <li><a href="./feed" rel="alternate" type="application/rss+xml">RSS</a></li>
+        </ul>
+      </section>
+
+      <p id="author" class="byline" itemprop="author" itemscope itemtype="http://schema.org/Person">Made by <span itemprop="name"><a href="./about/" rel="author" itemprop="url">Kyo Nagashima</a></span>.</p>
+    </footer>
+  </body>
+</html>
+*/}).toString().split('\n').slice(1, -1).join('\n');
+    var preview_file = path.resolve(__dirname, '../tmp/__preview.html');
+    var root = 'file:///' + path.resolve(__dirname, '../../build/').replace(/\\/g, '/') + '/';
+
+    var fn = path.basename(file, '.txt').replace(/\$/g, '$$');
+    var entry = fs.readFileSync(file, 'UTF-8').split('\n');
+    var title = entry.shift().replace(/\$/g, '$$$$');
+    var body = entry.join('\n').replace(/\$/g, '$$$$');
+
+    if (!/<\/\w+>\s*$/.test(body)) {
+      body = marked(body, {
+        langPrefix: 'language-'
+      });
+    }
+
+    body = body.replace(/="\//g, '="./');
+    preview = preview.replace(/<%ROOT%>/g, root).replace(/<%FN%>/g, fn).replace(/<%TITLE%>/g, title).replace(/<%BODY%>/g, body);
+    fs.writeFileSync(preview_file, preview);
+    grunt.log.writeln('File "' + preview_file + '" created.');
+    grunt.util.spawn({
+      cmd: 'open',
+      args: [preview_file]
+    }, function (error, result, code) {
+      if (error) {
+        return done(error);
+      }
+
+      done();
+    });
+  });
+};
