16b7edc3455316493da69610ece75e734b5c8f2b
Kyo Nagashima <kyo@hail2u.net>
2014-12-05 23:36:38 +0900

    Toggle appropriate class name

--- src/js/debug/show-column.js
+++ src/js/debug/show-column.js
@@ -1,7 +1,7 @@
 document.body.addEventListener('dblclick', function () {
-  if (/\bdebug\b/.test(this.className)) {
+  if (/\bshow-column\b/.test(this.className)) {
     this.className = this.className.replace(/ \bdebug\b/, '');
   } else {
-    this.className += ' debug';
+    this.className += ' show-column';
   }
 }, false);
