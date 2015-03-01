795ed9538f892978032fb73b6201d94171079648
Kyo Nagashima <hail2u@gmail.com>
2015-03-01 18:09:30 +0900

    Add `has-image` class with full width image

--- src/js/toggle-eyecatch.js
+++ src/js/toggle-eyecatch.js
@@ -1,22 +1,24 @@
 (function (d) {
   'use strict';
 
-  var toggle = function (eyecatch, evt) {
+  var toggle = function (eyecatch, article, evt) {
     if (eyecatch.hidden) {
       eyecatch.hidden = false;
     } else {
       eyecatch.hidden = true;
     }
 
+    article.classList.toggle('has-image');
     evt.preventDefault();
     evt.stopPropagation();
   };
 
   var init = function () {
+    var article = d.getElementById('introduction');
     var eyecatch = d.querySelector('.full-width');
     d.querySelector('.section-footer').addEventListener(
       'click',
-      toggle.bind(null, eyecatch),
+      toggle.bind(null, eyecatch, article),
       false
     );
   };
