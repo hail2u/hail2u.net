de6aba45468156b3b1d4688b737cbaad29364ec6
Kyo Nagashima <hail2u@gmail.com>
2015-05-10 19:12:04 +0900

    Split polyfills

---
+++ src/js/polyfill/string/endswith.js
@@ -0,0 +1,17 @@
+"use strict";
+
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function (search, position) {
+    var index = 0;
+    var subject = this.toString();
+
+    if (position === undefined || position > subject.length) {
+      position = subject.length;
+    }
+
+    position -= search.length;
+    index = subject.lastIndexOf(search, position);
+
+    return index !== -1 && index === position;
+  };
+}
