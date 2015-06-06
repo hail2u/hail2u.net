696d29fb723da02ece8d6a65d1f30dcf23325775
Kyo Nagashima <hail2u@gmail.com>
2015-06-07 04:05:07 +0900

    Remove obsolete Grunt task loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -519,7 +519,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("feedmix");
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-contrib-connect");
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-contrib-uglify");
