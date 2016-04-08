2b3147b3874d3901c75a1f68b245d2ee156fff89
Kyo Nagashima <hail2u@gmail.com>
2016-04-08 15:59:06 +0900

    Refactor

--- bin/styleguide.js
+++ bin/styleguide.js
@@ -10,18 +10,22 @@ var path = {
 };
 var site = "https://hail2u.net/";
 
-var c = fs.readFileSync("src/css/test.html", "utf-8");
-c = c.replace(/(href|src)(=)(")(.*?)(")/g, function (m, at, eq, oq, url, cq) {
-  if (url.indexOf(site) === 0) {
-    url = url.substr(site.length - 1);
-  } else if (url.indexOf(path.img) === 0) {
-    url = "/images" + url.substr(path.img.length - 1);
-  } else if (url.indexOf(path.js) === 0) {
-    url = "/scripts" + url.substr(path.js.length - 1);
-  } else if (/^[\w-]+\.[\w^]+$/.test(url)) {
-    url = "/styles/" + url;
-  }
+fs.outputFileSync(
+  "dist/about/style-guide/index.html",
+  fs.readFileSync(
+    "src/css/test.html",
+    "utf-8"
+  ).replace(/(href|src)(=)(")(.*?)(")/g, function (m, att, eq, oq, url, cq) {
+    if (url.indexOf(site) === 0) {
+      url = url.substr(site.length - 1);
+    } else if (url.indexOf(path.img) === 0) {
+      url = "/images" + url.substr(path.img.length - 1);
+    } else if (url.indexOf(path.js) === 0) {
+      url = "/scripts" + url.substr(path.js.length - 1);
+    } else if (/^[\w-]+\.[\w^]+$/.test(url)) {
+      url = "/styles/" + url;
+    }
 
-  return at + eq + oq + url + cq;
-});
-fs.outputFileSync("dist/about/style-guide/index.html", c);
+    return att + eq + oq + url + cq;
+  })
+);
