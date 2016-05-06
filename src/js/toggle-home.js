aa7037d96a3a0223088b5682497a8fac91a73285
Kyo Nagashima <hail2u@gmail.com>
2016-05-06 16:24:36 +0900

    Wrap First Heading around Section Footer
    This requires markup changes.

--- src/js/toggle-home.js
+++ src/js/toggle-home.js
@@ -3,16 +3,21 @@
 (function (d) {
   var className = "is-used-at-home";
 
-  function toggle(logo, evt) {
+  function toggle(logo, sectionFooter, evt) {
     evt.preventDefault();
     evt.stopPropagation();
     logo.classList.toggle(className);
+    sectionFooter.classList.toggle(className);
     evt.target.classList.toggle(className);
   }
 
   d.querySelector(".first-heading").addEventListener(
     "click",
-    toggle.bind(null, d.querySelector(".logo")),
+    toggle.bind(
+      null,
+      d.querySelector(".logo"),
+      d.querySelector(".section-footer")
+    ),
     false
   );
 })(document);
