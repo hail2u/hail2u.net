b03c078db31aad62a50b97867da8e05ae4308869
Kyo Nagashima <hail2u@gmail.com>
2016-05-07 16:28:52 +0900

    Build

--- scripts/toggle-home.js
+++ scripts/toggle-home.js
@@ -1,18 +1,25 @@
 "use strict";
 
 (function (d) {
-  var className = "is-used-at-home";
+  var className = "at-home";
 
-  function toggle(logo, evt) {
+  function toggle(globalHeader, logo, siteNavigation, evt) {
     evt.preventDefault();
     evt.stopPropagation();
+    globalHeader.classList.toggle(className);
     logo.classList.toggle(className);
+    siteNavigation.classList.toggle(className);
     evt.target.classList.toggle(className);
   }
 
   d.querySelector(".first-heading").addEventListener(
     "click",
-    toggle.bind(null, d.querySelector(".logo")),
+    toggle.bind(
+      null,
+      d.querySelector(".global-header"),
+      d.querySelector(".logo"),
+      d.querySelector(".site-navigation")
+    ),
     false
   );
 })(document);
