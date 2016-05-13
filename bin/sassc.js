5f8a36bc71a4e0026f6642240de197e4d6d7edd2
Kyo Nagashima <hail2u@gmail.com>
2016-05-14 00:09:01 +0900

    Use sassc through wrapper script

---
+++ bin/sassc.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var spawn = require("child_process").spawnSync;
+var which = require("which").sync;
+
+var cssExt = ".css";
+var destDir = "../tmp/";
+var scssExt = ".scss";
+var srcDir = "../src/css/";
+
+function sl(p) {
+  return p.replace(/\\/g, "/");
+}
+
+destDir = path.resolve(__dirname, destDir);
+srcDir = path.resolve(__dirname, srcDir);
+fs.readdirSync(srcDir).forEach(function (input) {
+  var basename = path.basename(input, scssExt);
+  var sassc;
+
+  if (path.extname(input) !== scssExt || basename.startsWith("_")) {
+    return;
+  }
+
+  sassc = spawn(which("sassc"), [
+    "--precision=3",
+    sl(path.join(srcDir, input)),
+    sl(path.join(destDir, basename + cssExt))
+  ], {
+    stdio: "inherit"
+  });
+
+  if (sassc.error) {
+    throw sassc.error;
+  }
+});
