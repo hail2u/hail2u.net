65444f547f3276e89cb06c433191acf2f89c30d6
Kyo Nagashima <hail2u@gmail.com>
2016-05-16 14:14:02 +0900

    Fix Site Navigation position at home on small screen

--- src/js/toggle-home.js
+++ src/js/toggle-home.js
@@ -3,12 +3,13 @@
 (function (d) {
   var className = "at-home";
 
-  function toggle(globalHeader, logo, siteNavigation, evt) {
+  function toggle(globalHeader, logo, siteNavigation, sectionFooter, evt) {
     evt.preventDefault();
     evt.stopPropagation();
     globalHeader.classList.toggle(className);
     logo.classList.toggle(className);
     siteNavigation.classList.toggle(className);
+    sectionFooter.classList.toggle(className);
     evt.target.classList.toggle(className);
   }
 
@@ -18,7 +19,8 @@
       null,
       d.querySelector(".global-header"),
       d.querySelector(".logo"),
-      d.querySelector(".site-navigation")
+      d.querySelector(".site-navigation"),
+      d.querySelector(".section-footer")
     ),
     false
   );
