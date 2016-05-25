cf56a6d18544a1b2b85323ebd3aae80198171bb9
Kyo Nagashima <hail2u@gmail.com>
2016-05-25 20:07:20 +0900

    Add wrapfix.js

---
+++ src/js/wrapfix.js
@@ -0,0 +1,27 @@
+/*!
+ * wrapfix.js
+ *
+ * LICENSE: http://hail2u.mit-license.org/2016
+ */
+"use strict";
+
+(function (d) {
+  var e;
+  var h;
+  var i;
+  var l;
+  var n = d.querySelectorAll("main h1");
+
+  for (i = 0, l = n.length; i < l; i += 1) {
+    e = n[i];
+
+    if (e.childNodes.length !== 1) {
+      continue;
+    }
+
+    h = e.textContent.split("");
+    e.textContent = h.slice(0, -2).concat(h.slice(-2).map(function (c) {
+      return "\uFEFF" + c;
+    })).join("");
+  }
+})(document);
