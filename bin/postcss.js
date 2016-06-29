955e87e3dd03de317b68bc885f58ac96eaa099dc
Kyo Nagashima <hail2u@gmail.com>
2016-06-29 14:31:57 +0900

    Lint

--- bin/postcss.js
+++ bin/postcss.js
@@ -2,22 +2,26 @@
 
 "use strict";
 
+var csswring = require("csswring");
 var eachLimit = require("async").eachLimit;
 var fs = require("fs");
 var mkdirp = require("mkdirp");
+var mqpacker = require("css-mqpacker");
 var os = require("os");
 var path = require("path");
-var postcss = require("postcss")([
-  require("postcss-round-float")(),
-  require("css-mqpacker")({
-    sort: true
-  }),
-  require("csswring")()
-]);
+var postcss = require("postcss");
+var roundFloat = require("postcss-round-float");
 
 var cpuNum = Math.max(1, os.cpus().length - 1);
 var cssExt = ".css";
 var minExt = ".min";
+var processor = postcss([
+  roundFloat(),
+  mqpacker({
+    sort: true
+  }),
+  csswring()
+]);
 var tmpdir = "../tmp/";
 
 tmpdir = path.resolve(__dirname, tmpdir);
@@ -31,7 +35,7 @@ eachLimit(fs.readdirSync(tmpdir), cpuNum, function (input, next) {
 
   input = path.join(tmpdir, input);
   output = path.join(tmpdir, basename + minExt + cssExt);
-  postcss.process(fs.readFileSync(input, "utf8")).then(function (result) {
+  processor.process(fs.readFileSync(input, "utf8")).then(function (result) {
     mkdirp.sync(path.dirname(output));
     fs.writeFileSync(output, result.css);
     next();
