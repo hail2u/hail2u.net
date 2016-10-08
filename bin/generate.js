9b476f9d68e2eae5d57d215a07a29189a4b1cfa9
Kyo Nagashima <hail2u@gmail.com>
2016-10-08 16:09:48 +0900

    Generate Home based on articles cache

--- bin/generate.js
+++ bin/generate.js
@@ -9,12 +9,10 @@ const minimist = require("minimist");
 const mkdirp = require("mkdirp");
 const mustache = require("mustache");
 const os = require("os");
-const parseXML = require("xml2js").parseString;
 const path = require("path");
 const sprintf = require("sprintf").sprintf;
 
 const articleCache = "../cache/articles.json";
-const blogFeed = "../dist/blog/feed";
 const blogFiles = [
   {
     src: "../src/html/blog/index.mustache"
@@ -46,7 +44,6 @@ const entityMap = {
   "<": "&lt;",
   ">": "&gt;"
 };
-const homeFeed = "../src/index.rss";
 const metadataFile = "../src/html/metadata.json";
 const partialDir = path.join(__dirname, "../src/html/partial");
 const templateDir = path.resolve(__dirname, "../src/html/");
@@ -78,52 +75,10 @@ function extendObject(dest, src) {
   return dest;
 }
 
-function readRSS(file) {
-  var feed = {};
-
-  parseXML(fs.readFileSync(file, "utf8"), {
-    trim: true,
-    explicitArray: false
-  }, function (error, data) {
-    if (error) {
-      throw error;
-    }
-
-    feed = data.rss.channel;
-  });
-
-  feed.item.forEach(function (val) {
-    var date;
-    var yy;
-    var mm;
-    var dd;
-
-    if (val.link) {
-      val.link = val.link.replace(/https?:\/\/hail2u\.net\//, "/");
-    }
-
-    if (val.pubDate) {
-      date = new Date(val.pubDate);
-      yy = date.getFullYear();
-      mm = date.getMonth();
-      dd = date.getDate();
-      val.strPubDate = sprintf("%04d/%02d/%02d", yy, mm + 1, dd);
-      val.html5PubDate = sprintf(
-        "%04d-%02d-%02dT%02d:%02d:%02d+09:00",
-        yy, mm + 1, dd, date.getHours(), date.getMinutes(), date.getSeconds()
-      );
-    }
-  });
-
-  return feed;
-}
-
 function readArticles() {
   var articles = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, articleCache), "utf8")
-  );
-
-  articles.forEach(function (article, idx) {
+  ).map(function (article, idx, arr) {
     article.strPubDate = sprintf("%02d/%02d", article.month, article.day);
     article.html5PubDate = sprintf(
       "%04d-%02d-%02dT%02d:%02d:%02d+09:00",
@@ -133,13 +88,16 @@ function readArticles() {
 
     if (idx && this.y !== article.year) {
       article.isFirstInYear = true;
-      articles[idx - 1].isLastInYear = true;
+      arr[idx - 1].isLastInYear = true;
     }
 
     this.y = article.year;
+
+    return article;
   }, {
     y: true
   });
+
   articles[0].isFirstInYear = true;
   articles[articles.length - 1].isLastInYear = true;
 
@@ -153,26 +111,12 @@ function readMetadata(file, callback) {
     metadata = extendObject(metadata, JSON.parse(data));
 
     switch (path.relative(templateDir, file).replace(/\\/g, "/")) {
-    case "index.json":
-      metadata.articles = readRSS(path.resolve(__dirname, blogFeed));
-      metadata.articles.item.forEach(function (item) {
-        var imgs = item["content:encoded"].match(/<img.*?>/g);
-
-        if (imgs) {
-          item.image = imgs.shift().replace(
-            /src="https?:\/\/hail2u\.net/,
-            'src="'
-          );
-          item.hasImage = true;
-        }
-      });
-      metadata.articles.item[0].latest = true;
-      metadata.updates = readRSS(path.resolve(__dirname, homeFeed));
-
-      break;
-
     case "blog/index.json":
       metadata.articles = readArticles();
+
+      break;
+    case "index.json":
+      metadata.articles = readArticles().slice(0, 12);
 
       break;
     }
