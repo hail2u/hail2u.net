d30e4dc6ecbb09698513c21acbd49cfebea07d96
Kyo Nagashima <hail2u@gmail.com>
2016-12-25 01:21:49 +0900

    Remove minimist dependency

--- bin/upload.js
+++ bin/upload.js
@@ -3,13 +3,10 @@
 "use strict";
 
 const execFile = require("child_process").execFileSync;
-const minimist = require("minimist");
 const path = require("path");
 const which = require("which").sync;
 
-const argv = minimist(process.argv.slice(2), {
-  string: ["file"]
-});
+const file = process.argv[2];
 const cmd = which("git");
 const cwd = path.resolve(__dirname, "../dist/");
 const opts = {
@@ -17,6 +14,6 @@ const opts = {
   stdio: "inherit"
 };
 
-execFile(cmd, ["add", "--", path.relative(cwd, argv.file)], opts);
+execFile(cmd, ["add", "--", path.relative(cwd, file)], opts);
 execFile(cmd, ["commit", "--message=Upload"], opts);
 execFile(cmd, ["push", "origin", "gh-pages"], opts);
