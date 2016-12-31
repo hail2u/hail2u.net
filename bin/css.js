f61d2d2912a1fc2262a3126f4c949ecf5d9d2f34
Kyo Nagashima <hail2u@gmail.com>
2016-12-31 20:36:59 +0900

    Merge mincss.js into css.js

--- bin/css.js
+++ bin/css.js
@@ -2,20 +2,33 @@
 
 "use strict";
 
+const csswring = require("csswring");
 const each = require("async").each;
-const fs = require("fs-extra");
-const path = require("path");
 const execFile = require("child_process").execFile;
+const fs = require("fs-extra");
+const mqpacker = require("css-mqpacker");
+const path = require("path");
+const postcss = require("postcss");
+const roundFloat = require("postcss-round-float");
 const which = require("which").sync;
 
 const cssExt = ".css";
-const destDir = path.resolve(__dirname, "../tmp/");
+const minExt = ".min";
+const processor = postcss([
+  roundFloat(),
+  mqpacker({
+    sort: true
+  }),
+  csswring()
+]);
 const sassc = which("sassc");
 const scssExt = ".scss";
 const srcDir = path.resolve(__dirname, "../src/css/");
+const tmpDir = path.resolve(__dirname, "../tmp/");
 
 each(fs.readdirSync(srcDir), function (src, next) {
   const basename = path.basename(src, scssExt);
+  let dest = path.join(tmpDir, `${basename}${cssExt}`);
 
   if (path.extname(src) !== scssExt || basename.startsWith("_")) {
     return next();
@@ -23,9 +36,18 @@ each(fs.readdirSync(srcDir), function (src, next) {
 
   execFile(sassc, [
     path.join(srcDir, src).replace(/\\/g, "/"),
-    path.join(destDir, `${basename}${cssExt}`)
+    dest
   ], function (err) {
-    next(err);
+    if (err) {
+      next(err);
+    }
+
+    src = dest;
+    dest = path.join(tmpDir, `${basename}${minExt}${cssExt}`);
+    processor.process(fs.readFileSync(src, "utf8")).then(function (result) {
+      fs.outputFileSync(dest, result.css);
+      next();
+    });
   });
 }, function (err) {
   if (err) {
