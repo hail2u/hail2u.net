325fae8fd7aec57f52869ca3e5c1ce244aa84745
Kyo Nagashima <hail2u@gmail.com>
2017-02-19 20:02:35 +0900

    Use asynchronous functions

--- bin/sitemap.js
+++ bin/sitemap.js
@@ -4,11 +4,12 @@
 
 const fs = require("fs-extra");
 const path = require("path");
+const series = require("async").series;
 const xml2js = require("xml2js");
 
 const cache = path.resolve(__dirname, "../cache/articles.json");
 const dest = path.resolve(__dirname, "../dist/sitemap.xml");
-const documentsDir = path.resolve(__dirname, "../src/html/documents/");
+const documentsDir = path.resolve(__dirname, "../dist/documents/");
 const sitemap = {
   urlset: {
     $: {
@@ -25,19 +26,68 @@ const urls = [
   "/documents/"
 ];
 
-fs.readdirSync(documentsDir).forEach((f) => {
-  if (path.extname(f) !== ".html") {
+function addDocument(filename) {
+  if (filename === "index.html") {
     return false;
   }
 
-  urls.push(`/${path.basename(documentsDir)}/${f}`);
-});
-fs.readJSONSync(cache).forEach((a) => {
-  urls.push(a.link);
-});
-urls.forEach((u) => {
-  sitemap.urlset.url.push({
-    loc: `https://hail2u.net${u}`
+  if (path.extname(filename) !== ".html") {
+    return false;
+  }
+
+  urls.push(`/${path.basename(documentsDir)}/${filename}`);
+}
+
+function gatherDocuments(next) {
+  fs.readdir(documentsDir, (e, f) => {
+    if (e) {
+      return next(e);
+    }
+
+    f.forEach(addDocument);
+    next();
   });
+}
+
+function gatherBlogEntries(next) {
+  fs.readJSON(cache, (e, o) => {
+    if (e) {
+      return next(e);
+    }
+
+    o.forEach((a) => {
+      urls.push(a.link);
+    });
+    next();
+  });
+}
+
+function toAbsoluteURL(next) {
+  urls.forEach((u) => {
+    sitemap.urlset.url.push({
+      loc: `https://hail2u.net${u}`
+    });
+  });
+  next();
+}
+
+function saveSitemap(next) {
+  fs.outputFile(dest, new xml2js.Builder().buildObject(sitemap), (e) => {
+    if (e) {
+      return next(e);
+    }
+
+    next();
+  });
+}
+
+series([
+  gatherDocuments,
+  gatherBlogEntries,
+  toAbsoluteURL,
+  saveSitemap
+], (e) => {
+  if (e) {
+    throw e;
+  }
 });
-fs.outputFileSync(dest, new xml2js.Builder().buildObject(sitemap));
