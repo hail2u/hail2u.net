ef6b1eb070c82db0be0b5b0cf3d9a7f2704f901f
Kyo Nagashima <hail2u@gmail.com>
2017-02-21 09:15:09 +0900

    Build

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,1 +1,28 @@
-undefined
+"use strict";
+
+(function () {
+  document.body.addEventListener("dblclick", function () {
+    this.classList.toggle("js-debug-columns");
+  }, false);
+})();
+"use strict";
+
+(function () {
+  function toggle(eyecatch) {
+    if (eyecatch.hidden) {
+      eyecatch.hidden = false;
+    } else {
+      eyecatch.hidden = true;
+    }
+  }
+
+  document.querySelector("footer")
+    .addEventListener("click", toggle.bind(null, document.querySelector(".fill")), false);
+})();
+"use strict";
+
+(function () {
+  document.body.addEventListener("dblclick", function () {
+    this.classList.toggle("js-debug-elements");
+  }, false);
+})();
