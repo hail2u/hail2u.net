dc6172b4962021046bcf0b11e64c28bb0849af10
Kyo Nagashima <hail2u@gmail.com>
2017-04-29 14:36:50 +0900

    Always toggle class for body

--- src/js/toggle-column.js
+++ src/js/toggle-column.js
@@ -1,5 +1,5 @@
 "use strict";
 
-document.body.addEventListener("dblclick", (e) => {
-  e.target.classList.toggle("js-debug-columns");
+document.body.addEventListener("dblclick", () => {
+  document.body.classList.toggle("js-debug-columns");
 }, false);
