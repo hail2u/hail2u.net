e89f9b83676a283a6c6fcbe713cff9c6ddb4c51c
Kyo Nagashima <hail2u@gmail.com>
2017-11-18 10:38:57 +0900

    Build

--- js/defer.js
+++ js/defer.js
@@ -75,13 +75,3 @@ for (const time of document.querySelectorAll("time.js-reldate[datetime]")) {
     time.textContent = rel;
   }
 }
-/*!
- * unutm.js
- *
- * LICENSE: http://hail2u.mit-license.org/2013
- */
-"use strict";
-
-if (location.search) {
-  history.replaceState(null, "", `${location.pathname}${location.search.replace(/[?&]utm_[^&]+/g, "").replace(/^&/, "?")}${location.hash}`);
-}
