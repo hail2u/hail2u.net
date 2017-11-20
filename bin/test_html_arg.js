216f382c043dd358ae17aed9a5c78e47e0b44331
Kyo Nagashima <hail2u@gmail.com>
2017-11-20 12:19:13 +0900

    Test any html file

--- bin/test_html_arg.js
+++ bin/test_html_arg.js
@@ -8,17 +8,23 @@ const which = require("which").sync;
 
 const src = "../src/weblog/entries/";
 const dest = "../dist/blog/";
-const file = process.argv.slice(2).shift();
+let file = process.argv.slice(2).shift();
 
 process.chdir(__dirname);
-execFileSync(which("htmlhint"), [
-  "--config",
-  "../.htmlhintrc",
-  path.join(
+file = path.resolve("../", file);
+
+if (file.endsWith(".txt")) {
+  file = path.join(
     dest,
     path.relative(src, path.dirname(file)),
     `${path.basename(file, ".txt")}.html`
   )
+}
+
+execFileSync(which("htmlhint"), [
+  "--config",
+  "../.htmlhintrc",
+  file
 ], {
   stdio: "inherit"
 });
