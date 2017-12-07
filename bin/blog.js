hail2u/hail2u.net

commit a99fe3f86e269b260ee86e4d86c5582cc9f5c117

Don’t insert, replace with new
The script should not break the cache order.

bin/blog.js
@@ -108,10 +108,8 @@ function addArticle(article, articles) {
   const oldArticle = articles.findIndex(isDuplicate.bind(null, article.link));
 
   if (oldArticle !== -1) {
-    articles.splice(oldArticle, 1);
+    articles[oldArticle] = article;
   }
-
-  articles.unshift(article);
 
   return articles;
 }

commit 2da492b32b11008feb061ff54fb18a5326375681

Add new article to top
This bug introduced by a99fe3f86e.

bin/blog.js
@@ -107,10 +107,12 @@ function isDuplicate(link, value) {
 function addArticle(article, articles) {
   const oldArticle = articles.findIndex(isDuplicate.bind(null, article.link));
 
-  if (oldArticle !== -1) {
-    article.unixtime = articles[oldArticle].unixtime;
-    articles[oldArticle] = article;
+  if (oldArticle === -1) {
+    return [article].concat(articles);
   }
+
+  article.unixtime = articles[oldArticle].unixtime;
+  articles[oldArticle] = article;
 
   return articles;
 }