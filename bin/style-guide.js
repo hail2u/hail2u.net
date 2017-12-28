9d7dd65afb442b713e5a9e05ee3a8c2e057861ff
Kyo Nagashima <hail2u@gmail.com>
2017-12-28 15:37:03 +0900

    Name replace function
    An anonymous function should be avoided as much as possible.

--- bin/style-guide.js
+++ bin/style-guide.js
@@ -1,20 +1,19 @@
 const fs = require("fs-extra");
 
 const readStyleGuide = async () => fs.readFile("../src/css/test.html", "utf8");
-const modifyStyleGuide = contents => {
-  const dir = "../";
-  const url = "https://hail2u.net/";
+const url = "https://hail2u.net/";
+const dir = "../";
+const rewriteURL = (m, a, e, o, u, c) => {
+  if (u.startsWith(url)) {
+    u = u.substr(url.length - 1);
+  } else if (u.startsWith(dir)) {
+    u = u.substr(dir.length - 1);
+  }
 
-  return contents.replace(/\b(href|src)(=)(")(.*?)(")/g, (m, a, e, o, u, c) => {
-    if (u.startsWith(url)) {
-      u = u.substr(url.length - 1);
-    } else if (u.startsWith(dir)) {
-      u = u.substr(dir.length - 1);
-    }
-
-    return `${a}${e}${o}${u}${c}`;
-  });
+  return `${a}${e}${o}${u}${c}`;
 };
+const modifyStyleGuide = contents =>
+  contents.replace(/\b(href|src)(=)(")(.*?)(")/g, rewriteURL);
 const writeStyleGuide = async contents =>
   fs.outputFile("../dist/style-guide/index.html", contents);
 const main = async () => {
