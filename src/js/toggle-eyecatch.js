49d8276b9319ad87c68c0d6641e426c55114a7cc
Kyo Nagashima <hail2u@gmail.com>
2018-07-24 18:56:28 +0900

    Return early

--- src/js/toggle-eyecatch.js
+++ src/js/toggle-eyecatch.js
@@ -3,7 +3,9 @@ const eyecatch = document.querySelector("body > main > article > h1 + figure");
 document.querySelector("footer").addEventListener("click", () => {
   if (eyecatch.hidden) {
     eyecatch.hidden = false;
-  } else {
-    eyecatch.hidden = true;
+
+    return;
   }
+
+  eyecatch.hidden = true;
 });
