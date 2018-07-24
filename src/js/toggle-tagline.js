5499c0a7541459a9fab0227d0e538c59bf445d9d
Kyo Nagashima <hail2u@gmail.com>
2018-07-24 22:51:21 +0900

    Test long tagline
    The tagline length is not static. So long version should be tested as
    well.

--- src/js/toggle-tagline.js
+++ src/js/toggle-tagline.js
@@ -9,3 +9,10 @@ document.querySelector("main h1").addEventListener("click", () => {
 
   tagline.hidden = true;
 });
+
+tagline.addEventListener("click", () => {
+  const current = tagline.textContent;
+  const next = tagline.dataset.long;
+  tagline.textContent = next;
+  tagline.dataset.long = current; 
+});
