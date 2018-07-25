69d7cd40f9b7ed85a3ea745f12f8a349dde3a167
Kyo Nagashima <hail2u@gmail.com>
2018-07-25 15:37:33 +0900

    Build

--- js/toggle-tagline.js
+++ js/toggle-tagline.js
@@ -9,3 +9,15 @@ document.querySelector("main h1").addEventListener("click", () => {
 
   tagline.hidden = true;
 });
+
+tagline.addEventListener("click", () => {
+  if (tagline.dataset.multi === "yes") {
+    tagline.parentNode.removeChild(tagline.parentNode.lastChild);
+    tagline.dataset.multi = "no";
+
+    return;
+  }
+
+  tagline.parentNode.appendChild(tagline.cloneNode(true));
+  tagline.dataset.multi = "yes";
+});
