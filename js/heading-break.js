e1bf186bb4294b0359e15b76973689315872306c
Kyo Nagashima <hail2u@gmail.com>
2018-08-13 18:44:43 +0900

    Build

---
+++ js/heading-break.js
@@ -0,0 +1,19 @@
+/*!
+ * heading-break.js
+ * LICENSE: http://hail2u.mit-license.org/2018
+ */
+
+const addWordJoiner = (added, value, index) => {
+  if (index > 3) {
+    return `${value}${added}`;
+  }
+
+  return `${value}\u2060${added}`;
+};
+
+for (const heading of document.querySelectorAll("main > article > h1")) {
+  heading.textContent = heading.textContent
+    .split("")
+    .reverse()
+    .reduce(addWordJoiner);
+}
