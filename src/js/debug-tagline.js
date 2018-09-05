e1a8ef02cc008d4adacfde73e1f17ee62dfdd77f
Kyo Nagashima <hail2u@gmail.com>
2018-09-05 09:34:31 +0900

    Test Tagline with long paragraph
    The content of Tagline should be one paragraph.

--- src/js/debug-tagline.js
+++ src/js/debug-tagline.js
@@ -1,32 +1,16 @@
 const tagline = document.querySelector("p");
-const taglineDebugClass = "js-debug-multiline-tagline";
 
 document.querySelector("main h1").addEventListener("click", () => {
   if (tagline.hidden) {
     tagline.hidden = false;
-
-    if (tagline.classList.contains(taglineDebugClass)) {
-      tagline.nextElementSibling.hidden = false;
-    }
-
     return;
   }
 
   tagline.hidden = true;
-
-  if (tagline.classList.contains(taglineDebugClass)) {
-    tagline.nextElementSibling.hidden = true;
-  }
 });
 
 tagline.addEventListener("click", () => {
-  tagline.classList.toggle(taglineDebugClass);
-
-  if (!tagline.classList.contains(taglineDebugClass)) {
-    tagline.parentNode.removeChild(tagline.parentNode.lastChild);
-
-    return;
-  }
-
-  tagline.parentNode.appendChild(tagline.cloneNode(true));
+  const oldTagline = tagline.textContent;
+  tagline.textContent = tagline.dataset.longtagline;
+  tagline.dataset.longtagline = oldTagline;
 });
