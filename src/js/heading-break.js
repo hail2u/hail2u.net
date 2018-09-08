da5b2df236ee613f78186c030dc8cb5de0faa2ab
Kyo Nagashima <hail2u@gmail.com>
2018-09-08 12:15:24 +0900

    Enhance progressively
    `heading-break.js` uses `u` flag in rgexp. So it should be enabled only
    on a supported browser.

--- src/js/heading-break.js
+++ src/js/heading-break.js
@@ -2,29 +2,27 @@
  * heading-break.js
  * LICENSE: http://hail2u.mit-license.org/2018
  */
-const addWordJoiner = (added, value, index) => {
-  if (index > 3) {
-    return `${value}${added}`;
+if (typeof RegExp.prototype.unicode === "boolean") {
+  const addWordJoiner = (added, value, index) => {
+    if (index > 3) {
+      return `${value}${added}`;
+    }
+
+    return `${value}\u2060${added}`;
+  };
+
+  const headingBreak = () => {
+    for (const heading of document.querySelectorAll("main > article > h1")) {
+      heading.textContent = heading.textContent
+        .match(/./gu)
+        .reverse()
+        .reduce(addWordJoiner);
+    }
+  };
+
+  if (document.readyState !== "loading") {
+    headingBreak();
+  } else {
+    document.addEventListener("DOMContentLoaded", headingBreak);
   }
-
-  return `${value}\u2060${added}`;
-};
-
-const headingBreak = () => {
-  if (typeof RegExp.prototype.unicode !== "boolean") {
-    return;
-  }
-
-  for (const heading of document.querySelectorAll("main > article > h1")) {
-    heading.textContent = heading.textContent
-      .match(/./gu)
-      .reverse()
-      .reduce(addWordJoiner);
-  }
-};
-
-if (document.readyState !== "loading") {
-  headingBreak();
-} else {
-  document.addEventListener("DOMContentLoaded", headingBreak);
 }
