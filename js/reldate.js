ff63cb1267f1d83c76c47db8db27ffbf2765d960
Kyo Nagashima <hail2u@gmail.com>
2018-09-14 15:06:37 +0900

    Build

--- js/reldate.js
+++ js/reldate.js
@@ -2,7 +2,7 @@
  * reldate.js
  * LICENSE: http://hail2u.mit-license.org/2016
  */
-const toRelativeDate = (from, to) => {
+const reldate = () => {
   const divisor = {
     day: 24,
     hour: 60,
@@ -21,54 +21,53 @@ const toRelativeDate = (from, to) => {
     second: "秒",
     year: "年"
   };
-
-  if (!Number.isInteger(to)) {
-    return;
-  }
-
-  let diff = Math.round((from - to) / divisor.second);
-
-  if (!Number.isInteger(diff) || diff < 0) {
-    return;
-  }
-
-  if (diff < divisor.minute / 2) {
-    return unit.now;
-  }
-
-  if (diff < divisor.minute) {
-    return `${diff}${unit.second}${suffix}`;
-  }
-
-  diff = Math.round(diff / divisor.minute);
-
-  if (diff < divisor.hour) {
-    return `${diff}${unit.minute}${suffix}`;
-  }
-
-  diff = Math.round(diff / divisor.hour);
-
-  if (diff < divisor.day) {
-    return `${diff}${unit.hour}${suffix}`;
-  }
-
-  diff = Math.round(diff / divisor.day);
-
-  if (diff < divisor.month) {
-    return `${diff}${unit.day}${suffix}`;
-  }
-
-  diff = Math.round(diff / divisor.month);
-
-  if (diff < divisor.year) {
-    return `${diff}${unit.month}${suffix}`;
-  }
-
-  return `${Math.round(diff / divisor.year)}${unit.year}${suffix}`;
-};
-
-const reldate = () => {
   const now = performance.timing.navigationStart + performance.now();
+
+  const toRelativeDate = (from, to) => {
+    if (!Number.isInteger(to)) {
+      return;
+    }
+
+    let diff = Math.round((from - to) / divisor.second);
+
+    if (!Number.isInteger(diff) || diff < 0) {
+      return;
+    }
+
+    if (diff < divisor.minute / 2) {
+      return unit.now;
+    }
+
+    if (diff < divisor.minute) {
+      return `${diff}${unit.second}${suffix}`;
+    }
+
+    diff = Math.round(diff / divisor.minute);
+
+    if (diff < divisor.hour) {
+      return `${diff}${unit.minute}${suffix}`;
+    }
+
+    diff = Math.round(diff / divisor.hour);
+
+    if (diff < divisor.day) {
+      return `${diff}${unit.hour}${suffix}`;
+    }
+
+    diff = Math.round(diff / divisor.day);
+
+    if (diff < divisor.month) {
+      return `${diff}${unit.day}${suffix}`;
+    }
+
+    diff = Math.round(diff / divisor.month);
+
+    if (diff < divisor.year) {
+      return `${diff}${unit.month}${suffix}`;
+    }
+
+    return `${Math.round(diff / divisor.year)}${unit.year}${suffix}`;
+  };
 
   for (const time of document.querySelectorAll("time.js-reldate[datetime]")) {
     const absolute = time.getAttribute("datetime");
