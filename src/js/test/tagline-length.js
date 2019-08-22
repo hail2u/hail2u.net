bd30c00b95a73781421b325c92510a15d9f35975
Kyo Nagashima <hail2u@gmail.com>
2019-08-22 13:09:55 +0900

    Refactor
    - Selector
    - data-* attribute names

--- src/js/test/tagline-length.js
+++ src/js/test/tagline-length.js
@@ -1,31 +1,31 @@
-const tagline = document.querySelector("p");
+const tagline = document.querySelector("body > header p:first-of-type");
 
 document.getElementById("test-tagline-length").addEventListener("click", () => {
   const oldTagline = tagline.textContent;
 
-  if (tagline.dataset.taglinestatus === "default") {
-    tagline.textContent = tagline.dataset.longtagline;
-    tagline.dataset.longtagline = oldTagline;
-    tagline.dataset.taglinestatus = "long";
+  if (tagline.dataset.status === "default") {
+    tagline.textContent = tagline.dataset.long;
+    tagline.dataset.long = oldTagline;
+    tagline.dataset.status = "long";
     return;
   }
 
-  if (tagline.dataset.taglinestatus === "long") {
-    tagline.textContent = tagline.dataset.shorttagline;
-    tagline.dataset.shorttagline = tagline.dataset.longtagline;
-    tagline.dataset.longtagline = oldTagline;
-    tagline.dataset.taglinestatus = "short";
+  if (tagline.dataset.status === "long") {
+    tagline.textContent = tagline.dataset.short;
+    tagline.dataset.short = tagline.dataset.long;
+    tagline.dataset.long = oldTagline;
+    tagline.dataset.status = "short";
     return;
   }
 
-  if (tagline.dataset.taglinestatus === "short") {
+  if (tagline.dataset.status === "short") {
     tagline.hidden = true;
-    tagline.dataset.taglinestatus = "hidden";
+    tagline.dataset.status = "hidden";
     return;
   }
 
   tagline.hidden = false;
-  tagline.textContent = tagline.dataset.shorttagline;
-  tagline.dataset.shorttagline = oldTagline;
-  tagline.dataset.taglinestatus = "default";
+  tagline.textContent = tagline.dataset.short;
+  tagline.dataset.short = oldTagline;
+  tagline.dataset.status = "default";
 });
