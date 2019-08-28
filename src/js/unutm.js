990b8d7307113e092b181dc20095bc44e7891815
Kyo Nagashima <hail2u@gmail.com>
2019-08-28 14:22:18 +0900

    Add unutm.js again

---
+++ src/js/unutm.js
@@ -0,0 +1,10 @@
+if (location.search) {
+  const search = location.search
+    .replace(/[?&]utm_[^&]+/g, "")
+    .replace(/^&/, "?");
+  history.replaceState(
+    null,
+    "",
+    `${location.pathname}${search}${location.hash}`
+  );
+}
