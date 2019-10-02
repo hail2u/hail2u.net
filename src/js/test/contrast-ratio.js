bbc5894dd71317db1ad1e0b1c09d4d71578a8f18
Kyo Nagashima <hail2u@gmail.com>
2019-10-02 16:05:56 +0900

    Refactor tests
    Unique globals. Modern API.

--- src/js/test/contrast-ratio.js
+++ src/js/test/contrast-ratio.js
@@ -1,37 +1,37 @@
-/* https://www.w3.org/TR/WCAG21/#dfn-relative-luminance */
-const getComponentLuminance = color => {
-  const sRGB = color / 255;
+const testContrastRatio = () => {
+  /* https://www.w3.org/TR/WCAG21/#dfn-relative-luminance */
+  const getComponentLuminance = color => {
+    const sRGB = color / 255;
 
-  if (sRGB <= 0.03928) {
-    return sRGB / 12.92;
-  }
+    if (sRGB <= 0.03928) {
+      return sRGB / 12.92;
+    }
 
-  return Math.pow((sRGB + 0.055) / 1.055, 2.4);
-};
+    return Math.pow((sRGB + 0.055) / 1.055, 2.4);
+  };
 
-const getRelativeLuminance = ([red, green, blue]) =>
-  0.2126 * getComponentLuminance(red) +
-  0.7152 * getComponentLuminance(green) +
-  0.0722 * getComponentLuminance(blue);
+  const getRelativeLuminance = ([red, green, blue]) =>
+    0.2126 * getComponentLuminance(red) +
+    0.7152 * getComponentLuminance(green) +
+    0.0722 * getComponentLuminance(blue);
 
-/* https://www.w3.org/TR/WCAG21/#dfn-contrast-ratio */
-const getContrast = element => {
-  const style = getComputedStyle(element);
-  const backgroundLuminance = getRelativeLuminance(style.backgroundColor.match(/\d+/g));
-  const foregroundLuminance = getRelativeLuminance(style.color.match(/\d+/g));
-  return (Math.max(backgroundLuminance, foregroundLuminance) + 0.05) /
-    (Math.min(backgroundLuminance, foregroundLuminance) + 0.05);
-};
+  /* https://www.w3.org/TR/WCAG21/#dfn-contrast-ratio */
+  const getContrast = element => {
+    const style = getComputedStyle(element);
+    const backgroundLuminance = getRelativeLuminance(style.backgroundColor.match(/\d+/g));
+    const foregroundLuminance = getRelativeLuminance(style.color.match(/\d+/g));
+    return (Math.max(backgroundLuminance, foregroundLuminance) + 0.05) /
+      (Math.min(backgroundLuminance, foregroundLuminance) + 0.05);
+  };
 
-const updateContrastRatio = () => {
   for (const color of document.querySelectorAll(".test-color li")) {
     const label = color.firstChild;
     const ratio = parseFloat(getContrast(color).toFixed(3));
     color.innerHTML = "";
-    color.appendChild(label);
-    color.appendChild(document.createElement("br"));
-    color.appendChild(document.createTextNode(ratio));
+    color.append(label);
+    color.append(document.createElement("br"));
+    color.append(ratio);
   }
 };
 
-window.addEventListener("load", updateContrastRatio);
+window.addEventListener("load", testContrastRatio);
--- src/js/test/relative-time.js
+++ src/js/test/relative-time.js
@@ -1,19 +1,24 @@
-const relativeTimeContainer = document.querySelector(".test-relative-time");
-[
-  1000 * 60 * 60 * 24 * 30 * 12 * 6,
-  1000 * 60 * 60 * 24 * 30 * 5,
-  1000 * 60 * 60 * 24 * 4,
-  1000 * 60 * 60 * 3,
-  1000 * 60 * 2,
-  1000 * 1
-].forEach(diff => {
-  const time = document.createElement("time");
-  time.classList.add("js-relative-time");
-  const dt = new Date(Date.now() - diff);
-  time.setAttribute("datetime", dt.toJSON());
-  time.append(dt.toLocaleString());
-  const li = document.createElement("li");
-  li.append(`${dt.toLocaleString()} => `);
-  li.append(time);
-  relativeTimeContainer.prepend(li);
-});
+const testRealtiveTime = () => {
+  const now = Date.now();
+  const container = document.querySelector(".test-relative-time");
+  [
+    1000 * 60 * 60 * 24 * 30 * 12 * 6,
+    1000 * 60 * 60 * 24 * 30 * 5,
+    1000 * 60 * 60 * 24 * 4,
+    1000 * 60 * 60 * 3,
+    1000 * 60 * 2,
+    1000 * 1
+  ].forEach(diff => {
+    const time = document.createElement("time");
+    time.classList.add("js-relative-time");
+    const dt = new Date(now - diff);
+    time.setAttribute("datetime", dt.toJSON());
+    time.append(dt.toLocaleString());
+    const li = document.createElement("li");
+    li.append(`${dt.toLocaleString()} => `);
+    li.append(time);
+    container.prepend(li);
+  });
+};
+
+testRealtiveTime();
