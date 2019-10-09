d7c573f3471cfea5909f746c0fd8fa5ac04ba148
Kyo Nagashima <hail2u@gmail.com>
2019-10-09 19:15:41 +0900

    Allow more extension for Photo
    I will upload a screenshot.

--- bin/photo.js
+++ bin/photo.js
@@ -5,22 +5,22 @@ const path = require("path");
 const runCommand = require("../lib/run-command");
 const sharp = require("sharp");
 
-const isPhoto = filename => {
-  if (path.extname(filename).toLowerCase() === ".jpg") {
+const isPhoto = ext => {
+  if ([".gif", ".jpeg", ".jpg", ".png", ".webp"].includes(ext)) {
     return true;
   }
 
   return false;
 };
 
-const generateFilename = dt => {
+const generateFilename = (dt, ext) => {
   const year = String(dt.getFullYear()).padStart(2, "0");
   const month = String(dt.getMonth() + 1).padStart(2, "0");
   const date = String(dt.getDate()).padStart(2, "0");
   const hours = String(dt.getHours()).padStart(2, "0");
   const minutes = String(dt.getMinutes()).padStart(2, "0");
   const seconds = String(dt.getSeconds()).padStart(2, "0");
-  return `${year}${month}${date}${hours}${minutes}${seconds}.jpg`;
+  return `${year}${month}${date}${hours}${minutes}${seconds}${ext}`;
 };
 
 const main = async () => {
@@ -29,13 +29,14 @@ const main = async () => {
   }
 
   const photo = process.argv.slice(2).shift();
+  const ext = path.extname(photo).toLowerCase();
 
-  if (!isPhoto(photo)) {
-    throw new Error("Photo file must be JPEG.");
+  if (!isPhoto(ext)) {
+    throw new Error("Photo file must be GIF, JPEG, PNG, or WebP.");
   }
 
   const [filename, git] = await Promise.all([
-    generateFilename(new Date()),
+    generateFilename(new Date(), ext),
     findCommand("git")
   ]);
   const src = path.join(config.src.photos, filename);
