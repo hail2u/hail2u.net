32762b5ec20bf6672ad5d4df2a77f9198382bd37
Kyo Nagashima <hail2u@gmail.com>
2019-10-13 21:26:48 +0900

    Revert "Allow more extension for Photo"
    This reverts commit d7c573f3471cfea5909f746c0fd8fa5ac04ba148.

--- bin/photo.js
+++ bin/photo.js
@@ -5,22 +5,22 @@ const runCommand = require("../lib/run-command");
 const sharp = require("sharp");
 const which = require("which");
 
-const isPhoto = ext => {
-  if ([".gif", ".jpeg", ".jpg", ".png", ".webp"].includes(ext)) {
+const isPhoto = filename => {
+  if (path.extname(filename).toLowerCase() === ".jpg") {
     return true;
   }
 
   return false;
 };
 
-const generateFilename = (dt, ext) => {
+const generateFilename = dt => {
   const year = String(dt.getFullYear()).padStart(2, "0");
   const month = String(dt.getMonth() + 1).padStart(2, "0");
   const date = String(dt.getDate()).padStart(2, "0");
   const hours = String(dt.getHours()).padStart(2, "0");
   const minutes = String(dt.getMinutes()).padStart(2, "0");
   const seconds = String(dt.getSeconds()).padStart(2, "0");
-  return `${year}${month}${date}${hours}${minutes}${seconds}${ext}`;
+  return `${year}${month}${date}${hours}${minutes}${seconds}.jpg`;
 };
 
 const main = async () => {
@@ -29,14 +29,13 @@ const main = async () => {
   }
 
   const photo = process.argv.slice(2).shift();
-  const ext = path.extname(photo).toLowerCase();
 
-  if (!isPhoto(ext)) {
-    throw new Error("Photo file must be GIF, JPEG, PNG, or WebP.");
+  if (!isPhoto(photo)) {
+    throw new Error("Photo file must be JPEG.");
   }
 
   const [filename, git] = await Promise.all([
-    generateFilename(new Date(), ext),
+    generateFilename(new Date()),
     which("git")
   ]);
   const src = path.join(config.src.photos, filename);
