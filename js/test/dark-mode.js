fe1194e2e665adb087b79beb3f702a67674dc53c
Kyo Nagashima <hail2u@gmail.com>
2019-10-21 22:05:04 +0900

    Build

--- js/test/dark-mode.js
+++ js/test/dark-mode.js
@@ -1,5 +1,5 @@
 document.querySelector(".js-test-dark-mode-button").addEventListener("click", () => {
-  document.documentElement.classList.toggle("js-dark-mode");
+  document.documentElement.classList.toggle("js-test-dark-mode");
 
   if (typeof testContrastRatio === "function") {
     testContrastRatio();
