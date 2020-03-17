aafc1b1a3b75cc5dfdb2002e91044f7eefd2ac0b
Kyo Nagashima <hail2u@gmail.com>
2020-03-17 09:31:29 +0900

    Bye dark mode
    For a small website, dark mode will kill website personality.

--- src/js/test/dark-mode.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".js-test-dark-mode-button").addEventListener("click", () => {
-  document.documentElement.classList.toggle("js-test-dark-mode");
-
-  if (typeof testContrastRatio === "function") {
-    testContrastRatio();
-  }
-});
-
-/* global testContrastRatio */
