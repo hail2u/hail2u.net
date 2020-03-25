daab62738e9472730a37067dec0a054f044d89d8
Kyo Nagashima <hail2u@gmail.com>
2020-03-25 18:44:23 +0900

    Build

--- js/test/column-and-element.js
+++ js/test/column-and-element.js
@@ -1,4 +1,4 @@
 document.querySelector(".js-test-column-and-element-button").addEventListener("click", () => {
-  document.body.classList.toggle("js-test-column");
-  document.body.classList.toggle("js-test-element");
+	document.body.classList.toggle("js-test-column");
+	document.body.classList.toggle("js-test-element");
 });
