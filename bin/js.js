61d2c26771e849e376a46cabb13b7a2c85de4212
Kyo Nagashima <hail2u@gmail.com>
2020-04-25 16:31:16 +0900

    Use Babel for transpiling
    Updating Google Closure Compiler is too hard on npm.

--- bin/js.js
+++ bin/js.js
@@ -1,39 +1,15 @@
-import googleClosureCompiler from "google-closure-compiler";
+import babel from "@babel/core";
 import config from "./index.js";
 import fs from "fs/promises";
 import getVersion from "../lib/get-version.js";
 
-const ClosureCompiler = googleClosureCompiler.jsCompiler;
-const compiler = new ClosureCompiler({
-	formatting: "PRETTY_PRINT",
-	isolationMode: "IIFE",
-	languageIn: "ECMASCRIPT_2019",
-	languageOut: "ECMASCRIPT5_STRICT"
-});
-
-const compileJS = (jsCode) => new Promise((resolve, reject) => {
-	compiler.run(jsCode, (exitCode, stdOut, stdErr) => {
-		if (exitCode !== 0) {
-			return reject(stdErr);
-		}
-
-		return resolve(stdOut[0].src);
-	});
-});
-
 const buildJS = async (file) => {
-	const code = await fs.readFile(file.src, "utf8");
 	const [version, compiled] = await Promise.all([
 		getVersion(),
-		compileJS([
-			{
-				sourceMap: null,
-				src: code
-			}
-		])
+		babel.transformFileAsync(file.src)
 	]);
 	const dest = file.dest.replace(/{{version}}/g, version);
-	await fs.writeFile(dest, compiled);
+	await fs.writeFile(dest, `(function(){${compiled.code}})();`);
 };
 
 Promise.all(config.files.js.map(buildJS)).catch((e) => {
