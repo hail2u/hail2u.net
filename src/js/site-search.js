431f4f0bb6e5b8e9e52dc5f5f390b1c0b4b610f4
Kyo Nagashima <hail2u@gmail.com>
2020-04-26 11:30:14 +0900

    Wrap long lines if possible
    Long lines should be wrapped in a reasonable point.

--- src/js/site-search.js
+++ src/js/site-search.js
@@ -1,9 +1,13 @@
 const showSiteSearchForm = () => {
-	const template = document.querySelector(".js-site-search");
-	template.content.querySelector(".js-site-search-form").addEventListener("submit", (event) => {
+	const doSearch = (event) => {
 		const query = event.srcElement.q;
 		query.value = `${query.value} site:hail2u.net`;
-	});
+	};
+
+	const template = document.querySelector(".js-site-search");
+	template.content
+		.querySelector(".js-site-search-form")
+		.addEventListener("submit", doSearch);
 	template.parentNode.insertBefore(template.content, template);
 	template.parentNode.removeChild(template);
 	const fallback = document.querySelector(".js-site-search-fallback");
