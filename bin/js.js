dfb1cd63fa69eff6381286218df74b46f48e4556
Kyo Nagashima <hail2u@gmail.com>
2020-04-27 14:37:21 +0900

    Configure Babel in inline
    It is suitable for this small project.

--- bin/js.js
+++ bin/js.js
@@ -6,7 +6,16 @@ import getVersion from "../lib/get-version.js";
 const buildJS = async (file) => {
 	const [version, compiled] = await Promise.all([
 		getVersion(),
-		babel.transformFileAsync(file.src)
+		babel.transformFileAsync(file.src, {
+			presets: [
+				[
+					"@babel/preset-env",
+					{
+						targets: "defaults"
+					}
+				]
+			]
+		})
 	]);
 	const dest = file.dest.replace(/{{version}}/g, version);
 	await fs.writeFile(dest, `(function(){${compiled.code}})();`);
