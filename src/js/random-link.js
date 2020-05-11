eced84dccba0979801e0c94878d8d28e8b9fce46
Kyo Nagashima <hail2u@gmail.com>
2020-05-11 15:46:21 +0900

    Support noscript user in random-link
    The placeholder for random-link has no meaning if a user
    disables JavaScript.

--- src/js/random-link.js
+++ src/js/random-link.js
@@ -2,7 +2,10 @@ const setRandomLink = () => {
 	const links = document.querySelectorAll(".js-random-link-link[href]");
 	const link = links[Math.floor(Math.random() * links.length)];
 	const href = link.getAttribute("href");
-	document.querySelector(".js-random-link").setAttribute("href", href);
+	const template = document.querySelector(".js-random-link");
+	template.content.querySelector("a").setAttribute("href", href);
+	template.parentNode.insertBefore(template.content, template);
+	template.parentNode.removeChild(template);
 };
 
 setRandomLink();
