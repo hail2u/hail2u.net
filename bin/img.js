e88778449811c161777c0a2472824413b415d9b7
Kyo Nagashima <hail2u@gmail.com>
2020-05-11 18:54:09 +0900

    Update dependencies
    The `bin/img.js` script now uses `@fiahfy/ico` instead of `to-ico`.

--- bin/img.js
+++ bin/img.js
@@ -1,8 +1,10 @@
 import config from "./config.js";
 import fs from "fs/promises";
+import ico from "@fiahfy/ico";
 import path from "path";
 import sharp from "sharp";
-import toIco from "to-ico";
+
+const { Ico, IcoImage } = ico;
 
 const generatePNG = async (file) => {
 	await sharp(file.src)
@@ -13,13 +15,22 @@ const generatePNG = async (file) => {
 
 const isFaviconSource = (filepath) => path.basename(filepath).startsWith("favicon-");
 
-const readPNG = (png) => fs.readFile(png, {
-	encoding: null
-});
+const appendIcon = async (icon, src) => {
+	const png = await fs.readFile(src, {
+		encoding: null
+	});
+	icon.append(IcoImage.fromPNG(png));
+};
+
+const toIco = async (srcs) => {
+	const icon = new Ico();
+	await Promise.all(srcs.map(appendIcon.bind(null, icon)));
+	return icon.data;
+};
 
 const main = async () => {
 	const pngs = await Promise.all(config.files.icon.map(generatePNG));
-	const srcs = await Promise.all(pngs.filter(isFaviconSource).map(readPNG));
+	const srcs = await Promise.all(pngs.filter(isFaviconSource));
 	const favicon = await toIco(srcs);
 	await fs.writeFile(config.dest.favicon, favicon);
 };
