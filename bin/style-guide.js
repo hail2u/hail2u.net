df2b141ef04ce029e04eab3f35a6d87306ed80b2
Kyo Nagashima <hail2u@gmail.com>
2020-05-21 10:42:01 +0900

    Refactor

--- bin/style-guide.js
+++ bin/style-guide.js
@@ -2,8 +2,9 @@ import config from "./config.js";
 import fs from "fs/promises";
 
 const main = async () => {
+	const reURLAttr = /\b(href|src)="(\.\.|https:\/\/hail2u\.net)(\/.*?)"/g;
 	const html = await fs.readFile(config.src.styleGuide, "utf8");
-	await fs.writeFile(config.dest.styleGuide, html.replace(/\b(href|src)="(\.\.|https:\/\/hail2u\.net)(\/.*?)"/g, "$1=\"$3\""));
+	await fs.writeFile(config.dest.styleGuide, html.replace(reURLAttr, "$1=\"$3\""));
 };
 
 main().catch((e) => {
