ffd016c4706177584f1e0c78d91b81dd4c35fa36
Kyo Nagashima <hail2u@gmail.com>
2020-06-25 13:26:40 +0900

    Update menu order

--- bin/txt.js
+++ bin/txt.js
@@ -315,10 +315,10 @@ const build = async (metadata, partials, file) => {
 	}
 
 	if (data.isHome) {
-		data.articles = data.articles.slice(0, 6);
+		data.articles = data.articles.slice(0, 4);
 		data.books = data.books.slice(0, 12);
-		data.documents = data.documents.slice(0, 6);
-		data.links = data.links.slice(0, 6);
+		data.documents = data.documents.slice(0, 1);
+		data.links = data.links.slice(0, 4);
 		data.photos = data.photos.slice(0, 12);
 		data.statuses = data.statuses.slice(0, 1);
 	}
