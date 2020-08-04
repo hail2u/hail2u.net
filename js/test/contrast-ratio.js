a0afb881548faec01618f287c88b85a06669aba6
Kyo Nagashima <hail2u@gmail.com>
2020-08-04 09:24:32 +0900

    Build

--- js/test/contrast-ratio.js
+++ js/test/contrast-ratio.js
@@ -39,12 +39,5 @@
 		}
 	};
 
-	window.testContrastRatio = update;
-
-	if (document.readyState === "loading") {
-		document.addEventListener("DOMContentLoaded", update);
-		return;
-	}
-
-	update();
+	window.addEventListener("load", update);
 })();
