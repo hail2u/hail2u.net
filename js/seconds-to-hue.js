9a78045a327a782eedc7c61c9bed450ca93936a8
Kyo Nagashima <hail2u@gmail.com>
2020-11-15 08:13:25 +0900

    Build

--- js/seconds-to-hue.js
+++
@@ -1,8 +0,0 @@
-const secondsToHue = (now) => {
-	const seconds = Math.floor(now.getSeconds() / 10);
-	const hue = seconds * 60 - 15;
-	const lightness = ["49%", "34%", "33%", "33%", "52%", "50%"];
-	document.documentElement.style = `--hue:${hue};--lightness:${lightness[seconds]}`;
-};
-
-secondsToHue(new Date());
