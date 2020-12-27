3f381ac1d98b5d69c70205d92fc3851d8400f0af
Kyo Nagashima <hail2u@gmail.com>
2020-12-27 15:26:09 +0900

    Refactor

--- src/js/test/column-and-element.js
+++ src/js/test/column-and-element.js
@@ -1,8 +1,8 @@
-const toggleColumnAndElementOverlays = () => {
+const testColumnAndElementOverlays = () => {
 	document.body.classList.toggle("js-test-column");
 	document.body.classList.toggle("js-test-element");
 };
 
 document
 	.querySelector(".js-test-column-and-element-button")
-	.addEventListener("click", toggleColumnAndElementOverlays);
+	.addEventListener("click", testColumnAndElementOverlays);
