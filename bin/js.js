6f6b5e8159968c745528602df2d6c42ec91490b1
Kyo Nagashima <hail2u@gmail.com>
2021-01-10 15:54:16 +0900

    Revert "Remove unnecessary build script"
    This reverts commit 61036bb9778d0be9e79e7b153df121b5ecfb5588.

---
+++ bin/js.js
@@ -0,0 +1,33 @@
+import babel from "@babel/core";
+import config from "../.config.js";
+import { outputFile } from "../lib/output-file.js";
+import { readJSONFile } from "../lib/json-file.js";
+
+const buildJS = async (version, file) => {
+	const dest = file.dest.replace(/\{\{version\}\}/gu, version);
+	const compiled = await babel.transformFileAsync(file.src, {
+		presets: [
+			[
+				"@babel/preset-env",
+				{
+					targets: "defaults",
+				},
+			],
+		],
+	});
+	const wrapped = `(function () {
+${compiled.code.trim()}
+})();`;
+	await outputFile(dest, wrapped);
+};
+
+const main = async () => {
+	const file = new URL("../package.json", import.meta.url);
+	const pkg = await readJSONFile(file);
+	await Promise.all(config.files.js.map(buildJS.bind(null, pkg.version)));
+};
+
+main().catch((e) => {
+	console.trace(e);
+	process.exitCode = 1;
+});
