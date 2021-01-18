7404fefbd31293565b49aa8b39767199e2fc23bf
Kyo Nagashima <hail2u@gmail.com>
2021-01-18 12:22:05 +0900

    Remove version number from Style Guide page
    It is not important information.

--- bin/style-guide.js
+++ bin/style-guide.js
@@ -1,15 +1,13 @@
 import config from "../.config.js";
 import fs from "fs/promises";
 import { outputFile } from "../lib/output-file.js";
-import { readJSONFile } from "../lib/json-file.js";
 
 const main = async () => {
 	const html = await fs.readFile(config.paths.src.styleGuide, "utf8");
-	const file = new URL("../package.json", import.meta.url);
-	const pkg = await readJSONFile(file);
-	const optimized = html
-		.replace(/\b(href|src)="(\.\.|https:\/\/hail2u\.net)(\/.*?)"/gu, '$1="$3"')
-		.replace(/<!-- version -->/gu, ` v${pkg.version}`);
+	const optimized = html.replace(
+		/\b(href|src)="(\.\.|https:\/\/hail2u\.net)(\/.*?)"/gu,
+		'$1="$3"'
+	);
 	await outputFile(config.paths.dest.styleGuide, optimized);
 };
 
