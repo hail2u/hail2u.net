72c910e53d2f492ab2682e0e60b8fe3cf142817a
Kyo Nagashima <hail2u@gmail.com>
2021-01-22 12:19:48 +0900

    Leave Style Guide page canonical URL as it is
    This should be full URL.

--- bin/style-guide.js
+++ bin/style-guide.js
@@ -2,11 +2,19 @@ import config from "../.config.js";
 import fs from "fs/promises";
 import { outputFile } from "../lib/output-file.js";
 
+const toAbsolutePath = (m, attr, prefix, path) => {
+	if (path === "/style-guide/") {
+		return m;
+	}
+
+	return `${attr}="${path}"`;
+};
+
 const main = async () => {
 	const html = await fs.readFile(config.paths.src.styleGuide, "utf8");
 	const optimized = html.replace(
 		/\b(href|src)="(\.\.|https:\/\/hail2u\.net)(\/.*?)"/gu,
-		'$1="$3"'
+		toAbsolutePath
 	);
 	await outputFile(config.paths.dest.styleGuide, optimized);
 };
