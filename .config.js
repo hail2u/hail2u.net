fe9af2de5d5d8aabf0f214092167ef2a568db2fb
Kyo Nagashima <hail2u@gmail.com>
2021-04-01 07:34:43 +0900

    Don’t use images for old books
    An image heavy page is not for this simple website.
    
    This commit also removes `append-next.js`, and also Babel.

--- .config.js
+++ .config.js
@@ -69,11 +69,6 @@ export default {
 				src: "src/bookshelf/index.mustache",
 			},
 			{
-				dest: "dist/bookshelf/log.html",
-				metadata: "src/bookshelf/log.json",
-				src: "src/bookshelf/log.mustache",
-			},
-			{
 				dest: "dist/documents/index.html",
 				metadata: "src/documents/index.json",
 				src: "src/documents/index.mustache",
@@ -139,12 +134,6 @@ export default {
 				dest: "dist/apple-touch-icon.png",
 				src: "src/img/icon.svg",
 				width: 180,
-			},
-		],
-		js: [
-			{
-				dest: "dist/js/append-next.{{version}}.js",
-				src: "src/js/append-next.js",
 			},
 		],
 	},
