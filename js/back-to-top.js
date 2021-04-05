9f5f5fa9bb5fea9f53f98e3ce19c1a224b218376
Kyo Nagashima <hail2u@gmail.com>
2021-04-05 20:04:43 +0900

    Build

--- js/back-to-top.js
+++
@@ -1,7 +0,0 @@
-const backToTop = () => {
-	if (location.hash === "#top") {
-		history.replaceState(null, "", `${location.pathname}${location.search}`);
-	}
-};
-
-window.addEventListener("hashchange", backToTop);
