53d4a49d42e0516be36f6451606219776cece52c
Kyo Nagashima <hail2u@gmail.com>
2021-09-12 20:38:44 +0900

    Handle abortion correctly
    Abotion should be determined by `type` property of an error.

--- bin/book.js
+++ bin/book.js
@@ -110,11 +110,11 @@ const main = async () => {
 		const url = `https://www.amazon.co.jp/exec/obidos/ASIN/${asin}/hail2unet-22`;
 		await openTwitter(`${title} ${url}`);
 	} catch (e) {
-		if (e) {
-			throw e;
+		if (e.type === "aborted") {
+			throw new Error("Amazon image server does not respond in 5s.");
 		}
 
-		throw new Error("Amazon image server does not respond in 5s.");
+		throw e;
 	} finally {
 		clearTimeout(abortID);
 	}
