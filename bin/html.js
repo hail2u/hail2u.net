5b484e57e9a79898ff173357182e46ae287ed8cf
Kyo Nagashima <hail2u@gmail.com>
2022-05-07 09:06:18 +0900

    Pick up first 9 items in sub-index pages
    In Bookshelf page, `loading=lazy` spams to an old browser.

--- bin/html.js
+++ bin/html.js
@@ -111,6 +111,18 @@ const mergeData = async (file, data) => {
 	};
 };
 
+const markFirstItem = (items) => {
+	const cloned = JSON.parse(JSON.stringify(items));
+	const firstItem = cloned.shift();
+	firstItem.isFirstInDate = true;
+	firstItem.isFirstInMonth = true;
+	firstItem.isFirstInYear = true;
+	return [
+		firstItem,
+		...cloned
+	];
+};
+
 const build = async (basic, partials, file) => {
 	const [
 		data,
@@ -119,6 +131,21 @@ const build = async (basic, partials, file) => {
 		mergeData(file, basic),
 		fs.readFile(file.src, "utf8")
 	]);
+
+	if (!data.isArticle && data.isBlog) {
+		data.latestArticles = data.articles.slice(0, 9);
+		data.articles = markFirstItem(data.articles.slice(9));
+	}
+
+	if (data.isBookshelf) {
+		data.latestBooks = data.books.slice(0, 9);
+		data.books = markFirstItem(data.books.slice(9));
+	}
+
+	if (data.isLinks) {
+		data.latestLinks = data.links.slice(0, 9);
+		data.links = markFirstItem(data.links.slice(9));
+	}
 
 	if (data.isHome) {
 		data.articles = data.articles.slice(0, 6);
