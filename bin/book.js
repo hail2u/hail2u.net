603c867c3335b30a92c4b7d8e6c0790c4b3d0a90
Kyo Nagashima <hail2u@gmail.com>
2023-01-23 09:44:35 +0900

    List title on Boookshelf
    In-page search does not work on images.

--- bin/book.js
+++ bin/book.js
@@ -75,7 +75,7 @@ const main = async () => {
 
 		const link = `https://www.amazon.co.jp/exec/obidos/ASIN/${asin}/hail2unet-22`;
 		const titleEscaped = escapeCharacters(title);
-		const body = `<a href="${link}"><img height="${metadata.height}" loading="lazy" src="${cover}" title="${titleEscaped}" width="${metadata.width}"></a>`;
+		const body = `<a href="${link}"><img height="${metadata.height}" src="${cover}" title="${titleEscaped}" width="${metadata.width}"></a>`;
 		const published = Date.now();
 		const dt = getDateDetails(published);
 		await outputJSONFile(file, [
