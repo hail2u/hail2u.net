0c72c69fc1422e89c57f2f7aace2d8f8d3d723af
Kyo Nagashima <hail2u@gmail.com>
2023-03-04 08:26:10 +0900

    Just copy more
    These files-to-copy should be in `static` directory.

--- config.js
+++ config.js
@@ -10,17 +10,13 @@ export default {
 	dest: {
 		article: "dist/blog/",
 		root: "dist/",
-		sitemap: "dist/sitemap.xml",
-		styleGuide: "dist/style-guide/index.html"
+		sitemap: "dist/sitemap.xml"
 	},
 	name,
 	src: {
-		assets: "assets/",
 		data: "data/",
 		draft: "drafts.html",
-		icon: "assets/img/icon.svg",
 		metadata: "metadata/",
-		styleGuide: "assets/css/test.html",
 		templates: "templates/"
 	},
 	version
