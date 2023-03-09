4ef7cbe22c40bd995667fec1c7d84b3614ff8095
Kyo Nagashima <hail2u@gmail.com>
2023-03-09 20:55:35 +0900

    Use import assertion
    This also changes ESLint parser.

--- config.js
+++ config.js
@@ -1,28 +1,16 @@
-import fs from "node:fs/promises";
-
-const file = new URL("./package.json", import.meta.url);
-const json = await fs.readFile(file, "utf8");
-const {
-	name,
-	version
-} = await JSON.parse(json);
-
-// import {
-// 	name,
-// 	version
-// } from './package.json' assert { type: 'json' };
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
 	dest: {
 		article: "dist/blog/",
 		root: "dist/"
 	},
-	name,
+	name: pkg.name,
 	src: {
 		data: "data/",
 		draft: "drafts.html",
 		metadata: "metadata/",
 		templates: "templates/"
 	},
-	version
+	version: pkg.version
 };
