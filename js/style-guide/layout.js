61eac2412e6c6b59b0c3efde6394558f0c168b4a
Kyo Nagashima <hail2u@gmail.com>
2023-03-17 15:12:34 +0900

    Build

---
+++ js/style-guide/layout.js
@@ -0,0 +1,7 @@
+/* layout.js | MIT License */
+
+document
+  .querySelector(".js-test-layout-button")
+  .addEventListener("click", () => {
+    document.body.classList.toggle("js-test-layout");
+  });
