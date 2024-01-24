f47ae723236a7825eb1c20e0fb2f9e0b03dce579
Kyo Nagashima <hail2u@gmail.com>
2024-01-24 16:08:39 +0900

    Remove author from alt text

--- bin/book.js
+++ bin/book.js
@@ -77,8 +77,7 @@ const main = async () => {
 
     const link = `https://www.amazon.co.jp/exec/obidos/ASIN/${asin}/hail2unet-22`;
     const titleEscaped = escapeCharacters(title);
-    const authorEscaped = escapeCharacters(author);
-    const body = `<a href="${link}"><img alt="${titleEscaped} / ${authorEscaped}" height="${metadata.height}" src="${cover}" width="${metadata.width}"></a>`;
+    const body = `<a href="${link}"><img alt="${titleEscaped}" height="${metadata.height}" src="${cover}" width="${metadata.width}"></a>`;
     const published = Date.now();
     const dt = getDateDetails(published);
     await outputJSONFile(file, [
