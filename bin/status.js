f551e5149fc8358df0e1895482f03369e89e795c
Kyo Nagashima <hail2u@gmail.com>
2024-03-13 19:57:32 +0900

    Resurrect various files

---
+++ bin/status.js
@@ -0,0 +1,46 @@
+import { outputJSONFile, readJSONFile } from "./lib/json-file.js";
+import config from "../config.js";
+import { getDateDetails } from "./lib/get-date-details.js";
+import { openTwitter } from "./lib/open-twitter.js";
+import path from "node:path";
+import { runCommand } from "./lib/run-command.js";
+import util from "node:util";
+
+const main = async () => {
+  const {
+    positionals: [text],
+  } = util.parseArgs({ allowPositionals: true });
+
+  if (!text) {
+    throw new Error("Only 1 argument is required.");
+  }
+
+  const file = path.join(config.dir.data, "statuses.json");
+  const statuses = await readJSONFile(file);
+  const published = Date.now();
+  const dt = getDateDetails(published);
+  await outputJSONFile(file, [
+    {
+      description: text,
+      link: `/statuses/#${published}`,
+      published,
+      ...dt,
+      title: text,
+      type: "status",
+    },
+    ...statuses,
+  ]);
+  await runCommand("git", ["add", "--", file]);
+  await runCommand("git", [
+    "commit",
+    "--message=Update status",
+    `--message=${text}`,
+  ]);
+  await openTwitter(text);
+};
+
+main().catch((e) => {
+  /* eslint-disable-next-line no-console */
+  console.trace(e);
+  process.exitCode = 1;
+});
---
+++ bin/subscription.js
@@ -0,0 +1,68 @@
+import { outputJSONFile, readJSONFile } from "./lib/json-file.js";
+import config from "../config.js";
+import { openTwitter } from "./lib/open-twitter.js";
+import path from "node:path";
+import { runCommand } from "./lib/run-command.js";
+import { shuffleArray } from "./lib/shuffle-array.js";
+import util from "node:util";
+
+const isSubscribed = (url, subscription) => url === subscription.url;
+
+const main = async () => {
+  const {
+    values: { feed, title, url },
+  } = util.parseArgs({
+    options: {
+      feed: {
+        short: "f",
+        type: "string",
+      },
+      title: {
+        short: "t",
+        type: "string",
+      },
+      url: {
+        short: "u",
+        type: "string",
+      },
+    },
+  });
+
+  if (!feed) {
+    throw new Error("--feed is required.");
+  }
+
+  if (!title) {
+    throw new Error("--title is required.");
+  }
+
+  if (!url) {
+    throw new Error("--url is required.");
+  }
+
+  const file = path.join(config.dir.data, "subscriptions.json");
+  const subscriptions = await readJSONFile(file);
+
+  if (subscriptions.find(isSubscribed.bind(null, url))) {
+    throw new Error(`${title} has already been subscribed.`);
+  }
+
+  const shuffled = shuffleArray([
+    {
+      feed,
+      link: url,
+      title,
+    },
+    ...subscriptions,
+  ]);
+  await outputJSONFile(file, shuffled);
+  await runCommand("git", ["add", "--", file]);
+  await runCommand("git", ["commit", `--message=Subscribe ${url}`]);
+  await openTwitter(`${title}の購読を開始 ${url}`);
+};
+
+main().catch((e) => {
+  /* eslint-disable-next-line no-console */
+  console.trace(e);
+  process.exitCode = 1;
+});
