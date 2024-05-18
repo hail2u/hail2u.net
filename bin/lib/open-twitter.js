bfbde62b4db876896b5d692bd843d9a7f61bcb4c
Kyo Nagashima <hail2u@gmail.com>
2024-05-18 14:20:41 +0900

    Use x.com for the platform formaly known as Twitter

--- bin/lib/open-twitter.js
+++ bin/lib/open-twitter.js
@@ -1,7 +1,7 @@
 import { runCommand } from "./run-command.js";
 
 const openTwitter = async (text) => {
-  const twitter = new URL("https://twitter.com/intent/tweet");
+  const twitter = new URL("https://x.com/intent/tweet");
   twitter.searchParams.append("text", text);
   await runCommand("open", [twitter.href]);
 };
