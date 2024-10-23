b7b3ba3d309dab98edab2d06ece48b5e95c4167b
Kyo Nagashima <hail2u@gmail.com>
2024-10-23 12:19:21 +0900

    Use fixed file for previewing
    Using `tmp` is overkill.

--- bin/draft.js
+++ bin/draft.js
@@ -8,8 +8,13 @@ import { selectDraft } from "./lib/select-draft.js";
 
 const main = async () => {
   const selected = await selectDraft();
-  const template = await fs.readFile(path.join(config.dir.template, "_draft.mustache"), "utf8");
-  const rendered = mustache.render(template, selected, null, { escape: escapeCharacters });
+  const template = await fs.readFile(
+    path.join(config.dir.template, "_draft.mustache"),
+    "utf8",
+  );
+  const rendered = mustache.render(template, selected, null, {
+    escape: escapeCharacters,
+  });
   await fs.mkdir(path.dirname(config.file.preview), { recursive: true });
   await fs.writeFile(config.file.preview, rendered);
   await runCommand("open", [config.file.preview]);
