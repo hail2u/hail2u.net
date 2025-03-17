c2aa72526f9a3855307904e9c94400c28b6c3434
Kyo Nagashima <hail2u@gmail.com>
2025-03-17 21:04:06 +0900

    Generate icon simply
    Icon can be generated in top-level.

--- bin/icon.js
+++ bin/icon.js
@@ -4,24 +4,21 @@ import ico from "@fiahfy/ico";
 import path from "node:path";
 import sharp from "sharp";
 
-const appendTo = async (icon, size) => {
-  const metadata = await sharp(config.file.icon).metadata();
-  const density = (size * metadata.density) / metadata.width;
-  const img = sharp(config.file.icon, { density });
-  const png = await img.resize(size).png().toBuffer();
+const toIcon = async (icon, size) => {
+  const density = (size * 72) / 16;
+  const png = await sharp(config.file.icon, { density })
+    .resize(size)
+    .flatten({ background: "hsl(48 48% 93%)" })
+    .png()
+    .toBuffer();
   icon.append(ico.IcoImage.fromPNG(png));
 };
 
-const generateIcon = async () => {
-  const sizes = [16, 24, 32, 48, 64, 128, 256];
-  const icon = new ico.Ico();
-  await Promise.all(sizes.map(appendTo.bind(null, icon)));
-  return icon;
-};
-
 const file = path.join(config.dir.dest, "favicon.ico");
-const [, icon] = await Promise.all([
+const sizes = [16, 24, 32, 48, 64, 128, 256];
+const icon = new ico.Ico();
+await Promise.all([
   fs.mkdir(path.dirname(file), { recursive: true }),
-  generateIcon(),
+  Promise.all(sizes.map(toIcon.bind(null, icon))),
 ]);
 await fs.writeFile(file, icon.data);
