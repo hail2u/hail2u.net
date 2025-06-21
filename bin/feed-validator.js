a263dc31d85067c841b16deb7ab39a2c3785cd58
Kyo Nagashima <hail2u@gmail.com>
2025-06-22 08:30:41 +0900

    Revert "Don’t validate feeds"
    This reverts commit c03e5703fcc01f19bb2a918136c6da9f22754652.

---
+++ bin/feed-validator.js
@@ -0,0 +1,91 @@
+import { XMLParser } from "fast-xml-parser";
+import config from "../config.js";
+import fs from "node:fs/promises";
+
+const cancelFetch = (abortController) => {
+  abortController.abort();
+};
+
+const parseXML = (xml) => {
+  const parser = new XMLParser({
+    arrayMode: /^error$/iu,
+    removeNSPrefix: true,
+  });
+  const json = parser.parse(xml);
+  return json.Envelope.Body.feedvalidationresponse.errors;
+};
+
+const validateFeed = async (feed) => {
+  const body = new URLSearchParams();
+  body.append("manual", 1);
+  body.append("output", "soap12");
+  body.append("rawdata", feed);
+  const abortController = new AbortController();
+  const abortID = setTimeout(
+    cancelFetch.bind(null, abortController),
+    config.test.timeout,
+  );
+
+  try {
+    const res = await fetch("https://validator.w3.org/feed/check.cgi", {
+      body,
+      method: "POST",
+      signal: abortController.signal,
+    });
+
+    if (!res.ok) {
+      return `Skipped. ${res.status} ${res.statusText}.`;
+    }
+
+    const xml = await res.text();
+    const json = await parseXML(xml);
+    const errorcount = parseInt(json.errorcount, 10);
+
+    if (errorcount === 0) {
+      return null;
+    }
+
+    return json.errorlist.error;
+  } catch (e) {
+    if (e.name === "AbortError") {
+      return `Skipped. W3C Feed Validator does not respond in ${config.test.timeout}s.`;
+    }
+
+    throw e;
+  } finally {
+    clearTimeout(abortID);
+  }
+};
+
+const formatMessage = (file, { column, line, msgcount, text }) =>
+  `${file}:${line}:${column}: ${text} (${msgcount}).`;
+
+const validate = async (file) => {
+  const feed = await fs.readFile(file, "utf8");
+  const messages = await validateFeed(feed);
+
+  if (!messages) {
+    return [];
+  }
+
+  if (typeof messages === "string") {
+    process.stdout.write(`${file}:1:1: ${messages}
+`);
+    return [];
+  }
+
+  return Promise.all(messages.map(formatMessage.bind(null, file)));
+};
+
+const isNotEmpty = (element) => element.length !== 0;
+
+const feeds = await Array.fromAsync(fs.glob(`${config.dir.dest}**/feed`));
+const results = await Promise.all(feeds.map(validate));
+const errors = results.flat();
+
+if (errors.length > 0) {
+  const errorFiles = results.filter(isNotEmpty);
+  process.stderr.write(errors.join("\n"));
+  process.stderr.write("\n\n");
+  throw new Error(`${errors.length} error(s) in ${errorFiles.length} file(s)`);
+}
