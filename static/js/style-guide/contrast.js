7d783abd3a94eaebeb850e741bc96f56b1ee2ce1
Kyo Nagashima <hail2u@gmail.com>
2025-06-24 08:16:22 +0900

    Remove unnecessary function
    Conversion to sRGB is only needed for stringify.

--- static/js/style-guide/contrast.js
+++ static/js/style-guide/contrast.js
@@ -1,20 +1,15 @@
 /* Test contrast | MIT License */
 import Color from "../lib/color.min.js";
 
-const toRGB = (color) => {
-  const c = new Color(color);
-  return c.to("srgb");
-};
-
 const showContrast = () => {
   const queryColorTip = ".test-color > tbody > tr > td:nth-child(2) .test";
   const colorTips = document.querySelectorAll(queryColorTip);
 
   for (const colorTip of colorTips) {
     const style = getComputedStyle(colorTip);
-    const background = toRGB(style.getPropertyValue("background-color"));
-    const foreground = toRGB(style.getPropertyValue("color"));
-    colorTip.textContent = background.toString({
+    const background = new Color(style.getPropertyValue("background-color"));
+    const foreground = new Color(style.getPropertyValue("color"));
+    colorTip.textContent = background.to("srgb").toString({
       format: {
         coords: ["<number>[0, 255]", "<number>[0, 255]", "<number>[0, 255]"],
         name: "rgb",
@@ -22,13 +17,16 @@ const showContrast = () => {
       precision: 0,
     });
     const contrastCell = colorTip.parentElement.nextElementSibling;
-    const ratio = Math.round(background.contrast(foreground, "WCAG21") * 1000) / 1000;
+    const ratio =
+      Math.round(background.contrast(foreground, "WCAG21") * 1000) / 1000;
 
     if (colorTip.classList.contains("js-test-color-flip")) {
-      const score = Math.round(foreground.contrast(background, "APCA") * 1000) / 1000;
+      const score =
+        Math.round(foreground.contrast(background, "APCA") * 1000) / 1000;
       contrastCell.lastChild.textContent = `${ratio} (${score})`;
     } else {
-      const score = Math.round(background.contrast(foreground, "APCA") * 1000) / 1000;
+      const score =
+        Math.round(background.contrast(foreground, "APCA") * 1000) / 1000;
       contrastCell.lastChild.textContent = `${ratio} (${score})`;
     }
   }
