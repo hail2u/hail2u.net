cfe849bb1610bb6bef76629a6f61d3fd20ef2199
Kyo Nagashima <hail2u@gmail.com>
2026-04-23 12:34:39 +0900

    Remove Showcase component
    I want to remove external images.

--- static/js/style-guide/showcase.js
+++
@@ -1,8 +0,0 @@
-/* Toggle Showcase mode | MIT License */
-const toggleShowcaseMode = () => {
-  document.querySelector(".js-test-showcase").classList.toggle("oneline");
-};
-
-document
-  .querySelector(".js-test-showcase-button")
-  .addEventListener("click", toggleShowcaseMode);
